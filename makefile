e4060fa2e34cc07140c1c44f12d439b8c288946e
vrodedanya <socurahlem@yandex.ru>
2020-10-16 19:37:57 +0300

    New project structure and port to c++
    Added DeltaTime to calculate changes per second
    Pthreads replaced by C++ standard threads

--- makefile
+++
@@ -1,4 +0,0 @@
-fire: fire.src/main.c
-	gcc fire.src/main.c -o fire -lm -lSDL2 -lpthread
-water: water.src/main.c
-	gcc water.src/main.c -o water -lm -lSDL2 -lpthread
--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,11 @@
-#include <stdlib.h>
-#include <pthread.h>
-#include <stdio.h>
+#include <iostream>
+#include <thread>
 #include <SDL2/SDL.h>
-#include <math.h>
-#include <time.h>
+#include <cmath>
+#include "DeltaTime.h"
 
-#define PART_QUANT 1500
-#define THREADS 128
+#define PART_QUANT 4000
+#define THREADS 4
 
 typedef struct
 {
@@ -30,9 +29,9 @@ typedef struct
 	SDL_Renderer* renderer;
 }render_storage;
 
-void spawn_particles(Particle* p, int center_x, int center_y, int radius_x, int radius_y, int exclude_radius)
+void spawn_particles(Particle* p, const int& center_x, const int& center_y, const int& radius_x, const int& radius_y, const int& exclude_radius)
 {
-	register int radius_buffer;
+	register int radius_buffer = 0;
 	register double angle;
 	for (register int i = 0 ; i < PART_QUANT ; ++i)
 	{
@@ -75,52 +74,50 @@ void init_particles(Particle* p)
 	}
 }
 
-void move_particles(Particle* p, SDL_Event event, double delta)
+void move_particles(particle_storage* data, const SDL_Event& event)
 {
 	register double speed;
-	for (register int i = 0 ; i < PART_QUANT ; ++i)
+	for (register int i = data->start ; i < data->end ; ++i)
 	{
-		if (p[i].t > 0)
+		if (data->p[i].t > 0)
 		{	
-			speed = delta * p[i].t * 2;
-			p[i].y -= speed;
-			if (sqrt(pow(p[i].x - event.motion.x,2) + pow(p[i].y - event.motion.y,2) <= 900))
+			speed = DeltaTime::delta * data->p[i].t * 5;
+			data->p[i].y -= speed;
+			if (sqrt(pow(data->p[i].x - event.motion.x,2) + pow(data->p[i].y - event.motion.y,2) <= 900))
 			{
-				if (p[i].x > event.motion.x)
+				if (data->p[i].x > event.motion.x)
 				{
-					p[i].x = sqrt(900 - pow(p[i].y - event.motion.y,2)) + event.motion.x;
+					data->p[i].x = sqrt(900 - pow(data->p[i].y - event.motion.y,2)) + event.motion.x;
 				}
 				else
 				{
-					p[i].x = -1 * sqrt(900 - pow(p[i].y - event.motion.y,2)) + event.motion.x;
+					data->p[i].x = -1 * sqrt(900 - pow(data->p[i].y - event.motion.y,2)) + event.motion.x;
 
 				}
 			}
 			else
 			{
-				p[i].x = p[i].x + (-3 + rand()%5);
+				data->p[i].x = data->p[i].x + (-300 + rand()%500) * DeltaTime::delta;
 			}
 		}
 	}
 }
-void* check_environment(void* thread_data)
+void check_environment(particle_storage* data)
 {
-	particle_storage* data = (particle_storage*) thread_data;
 	for (register int i = data->start ; i < data->end ; ++i)
 	{
-		data->p[i].e = 0;
+		int env = 0;
 		for (register int j = 0 ; j < PART_QUANT ; ++j)
 		{
 			if (i == j) continue;
-			if (sqrt(pow(data->p[i].x - data->p[j].x, 2) + pow(data->p[i].y - data->p[j].y, 2) <= 30))
+			if (sqrt(pow(data->p[i].x - data->p[j].x, 2) + pow(data->p[i].y - data->p[j].y, 2) <= 40))
 			{
-				++data->p[i].e;
+				env++;
 			}
 		}
-		if (data->p[i].e == 0) data->p[i].t -= 4;
-		else if (data->p[i].e >= 5) ++data->p[i].t;
+		if (env == 0) data->p[i].t -= 4;
+		else if (env >= 10) ++data->p[i].t;
 	}
-	pthread_exit(0);
 }
 
 int main()
@@ -135,7 +132,7 @@ int main()
 	Particle particles[PART_QUANT];
 	init_particles(particles);
 
-	pthread_t th[THREADS];
+	std::thread threads[THREADS];
 
 	particle_storage pd[THREADS];
 	for (int i = 0 ; i < THREADS ; ++i)
@@ -145,42 +142,45 @@ int main()
 		pd[i].end = (i + 1) * PART_QUANT / THREADS;
 	}
 
-	double time_start = clock(), time_end = clock() + 10;
-	double delta;
+	DeltaTime dt;
 
-	int isWork = 1;
+	bool isWork = true;
 	while(isWork)
 	{
-		delta = (time_end - time_start) / CLOCKS_PER_SEC;
-		time_start = clock();
+		dt.begin();
+
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 		SDL_RenderClear(renderer);	
-		for (int i = 0 ; i < THREADS ; ++i)
+		for (int i = 0 ; i < THREADS ; i++)
 		{
-			pthread_create(&th[i], NULL, check_environment, (void*) &pd[i]);
+			threads[i] = std::thread(check_environment, &pd[i]);
+
+		}
+		for (int i = 0 ; i < THREADS ; i++)
+		{
+			threads[i].join();
+			threads[i] = std::thread(move_particles, &pd[i], event);
+		}
+		for (int i = 0 ; i < THREADS ; i++)
+		{
+			threads[i].join();
 		}
 
-		for (int i = 0 ; i < THREADS ; ++i)
-		{
-			pthread_join(th[i], NULL);
-		}
-
-		spawn_particles(particles, 1920 / 2, 1080, 400, 10, 0);
-		move_particles(particles, event, delta);
-		draw_particles(renderer,particles);
+		spawn_particles(particles, 1920 / 2, 1080, 960, 30, 0);
+		draw_particles(renderer, particles);
 		
 		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) isWork = 0;
+			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) isWork = false;
 			if (event.type == SDL_MOUSEBUTTONDOWN)
 			{
 				if (event.button.button == SDL_BUTTON_LEFT) SDL_ShowCursor(0);
 				else SDL_ShowCursor(1);
 			}
-			if (event.type == SDL_QUIT) isWork = 0;
+			if (event.type == SDL_QUIT) isWork = false;
 		}
 		SDL_RenderPresent(renderer);
-		time_end = clock();
+		dt.end();
 	}
 	return 0;
 }
